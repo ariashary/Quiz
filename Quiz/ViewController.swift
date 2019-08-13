5ce4a503905fdad525c7743e670e0829f0585ba3
Muhammad Ashary <muh.ariashary@gmail.com>
2019-08-13 23:19:08 +0700

    Initial commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Muhammad Ashary on 12/08/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+    }
+    
+}
+
